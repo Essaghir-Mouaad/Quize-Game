13ccbc63ad8b6f120b46657f251e24af8097e156
mouaad <mouadessaghie7@gmail.com>
2024-12-27 18:33:06 +0100

    add games interfaces

--- server/routes/main.js
+++ server/routes/main.js
@@ -32,6 +32,14 @@ const blogs = [
 
 ];
 
+
+const game = {
+  name:"cascade",
+  description:"Face a barrage of fast-paced CSS microgames, inspired by the likes of WarioWare, that put your CSS knowledge to the test. Includes dozens of microgames focused on a wide range of CSS that will keep you on your toes",
+  Learning_Objectives:"Exercise your recall on a variety of CSS <code>features</code> ,  <code>including</code> <code>flexbox</code> , <code>grid</code> , <code>transform</code> , <code>direction</code> , <code>filter</code> , <code>counter</code> , <code>nth-child</code> , calc and more.",
+  Prerequisites:"For everyone from beginners to experts with a basic familiarity of CSS"
+}
+
 router.get("/", (req, res)=>{
   res.render("home");
 });
@@ -45,9 +53,23 @@ router.get("/games", (req, res) => {
   res.render("games");
 });
 
+router.get("/games/:game_name", (req, res)=>{
+  var path = _.lowerCase(req.params.game_name);
+  var found = false;
+
+  if (path === _.lowerCase(game.name)) {
+    res.render("game_name", {name: game.name, description: game.description, Learning_Objectives:game.Learning_Objectives, Prerequisites:game.Prerequisites});
+    found = truez
+  }
+})
+
+
+
 router.get("/blogs", (req, res) => {
   res.render("blogs", { blogs: blogs });
 });
+
+
 
 router.get("/blogs/:article", (req, res) => {
   var path = _.lowerCase(req.params.article);
